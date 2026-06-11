620c697b64edfdf4509751d9cd81a0366f8591e6
israelsouzaandrade-del <israel.souza.andrade@escola.pr.gov.br>
2026-06-11 13:55:12 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", () => {
+    mensagem.innerHTML =
+    "🌱 A agricultura sustentável é essencial para garantir alimentos, preservar a natureza e construir um futuro melhor para todos!";
+});
